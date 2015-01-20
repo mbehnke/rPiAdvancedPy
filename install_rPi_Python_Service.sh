b8e7d858d2817eb71f2ec0ce18084d50cce07f30
Michael Behnke <mbehnkeac@gmail.com>
2015-01-20 03:01:16 +0100

    Update install_rPi_Python_Service.sh

--- install_rPi_Python_Service.sh
+++ install_rPi_Python_Service.sh
@@ -3,4 +3,4 @@ sudo rm /etc/init.d/HVACAlgo
 wget https://raw.githubusercontent.com/mbehnke/rPiAdvancedPy/master/HVACAlgo.sh
 sudo mv HVACAlgo.sh /etc/init.d/
 sudo chmod 755 /etc/init.d/HVACAlgo.sh
-sudo update-rc.d HVACAlgo defaults
+sudo update-rc.d HVACAlgo.sh defaults
